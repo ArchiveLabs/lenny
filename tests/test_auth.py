909cc808ce58d8a65be9c5408eed50544fdb8b3c
Mek <michael.karpeles@gmail.com>
2025-08-05 17:15:34 -0700

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -3,5 +3,9 @@ import pytest
 from lenny.core import auth
 
 @pytest.fixture
-def test_cookie():
-    assert auth.create_session_cookie("example@archive.org") == "XXX"
+def test_cookie(monkeypatch):
+    monkeypatch.setattr(auth, "SEED", "123")
+    email = "example@archive.org"
+    cookie = auth.create_session_cookie(email)
+    assert cookie.startswith("ImV4YW1wbGVAYXJjaGl2ZS5vcmci")
+    assert auth.get_authenticated_email(cookie) == email
